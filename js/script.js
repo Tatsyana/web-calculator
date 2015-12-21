b380457842e176c0861b630e4063beace7cb7197
Tatsyana <justicia1990@mail.ru>
2015-12-21 20:12:04 +0300

    Added operator event.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 $(function input_number(){
+  $('.operator').click(function(){
+  
+    var calcInput = $('.calc-input');
+    var lastValue = calcInput.val(); 
+    var lastChar = lastValue[lastValue.length-1];
+    var operators = ['+', '-', '/', '*'];
+    if ($.inArray(lastChar, operators) != -1) {
+      lastValue = lastValue.substring(0, lastValue.length - 1);
+    }
+
+    if (lastValue == '0') {
+      lastValue = '';
+    }
+    
+    calcInput.val(lastValue + $(this).val());
+  });
+  
   $('.number').click(function(){
   
     var calcInput = $('.calc-input');
@@ -7,13 +24,15 @@ $(function input_number(){
       lastValue = '';
     }
     calcInput.val(lastValue + $(this).val());
-
-    
-    
   });
   
-    $('.clear').click(function(){
-      $('.calc-input').val("0");  
+  
+  
+  $('.clear').click(function(){
+    $('.calc-input').val("0");  
   });
+  
+  
+  
   
 });
