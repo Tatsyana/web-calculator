7e24d23c9dfc67f4a6d4087dde0f3cc32b1db464
Tatsyana <justicia1990@mail.ru>
2015-12-28 19:18:07 +0300

    Комметрирование кода.

--- js/script.js
+++ js/script.js
@@ -1,37 +1,46 @@
 $(function input_number(){
-  $('.operator').click(function(){
+  // Срабатывает, если нажата кнопка с оператором.
+  $('.operator').click(function(){ 
   
-    var calcInput = $('.calc-input');
-    var lastValue = calcInput.val(); 
-    var lastChar = lastValue[lastValue.length-1];
-    var operators = ['+', '-', '/', '*'];
-    if ($.inArray(lastChar, operators) != -1) {
-      lastValue = lastValue.substring(0, lastValue.length - 1);
+    var calcInput = $('.calc-input');                             // Строка для ввода.
+    var lastValue = calcInput.val();                              // Сохраняем значение строки ввода.
+    var lastChar = lastValue[lastValue.length-1];                 // Берем последний символ вводной строки.
+    var operators = ['+', '-', '/', '*'];                         // Определяем массив операторов.
+    if ($.inArray(lastChar, operators) != -1) {                   // Если последний символ - оператор.
+      lastValue = lastValue.substring(0, lastValue.length - 1);   // Удаляем последний символ, сохраняется только числовое значение.
     }
 
+    // Проверка если элемент не первый (не допускает +5, -6).
     if (lastValue != '0') {
-      calcInput.val(lastValue + $(this).val());
+      calcInput.val(lastValue + $(this).val());                   // this - элемент, по которому клик, Добавление к текущему значению кликнутой кнопки.
     }
     
   });
   
+   // Срабатывает, если нажата кнопка с цифрой.
   $('.number').click(function(){
-  
-    var calcInput = $('.calc-input');
-    var lastValue = calcInput.val(); 
+                                                                   
+    var calcInput = $('.calc-input');                             // Строка для ввода.
+    var lastValue = calcInput.val();                              // Сохраняем значение строки ввода.
+    
+    // Проверка, для корректного отображения введенного значения без 0 спереди (пустая строка).
     if (lastValue == '0') {
       lastValue = '';
     }
-    calcInput.val(lastValue + $(this).val());
+    calcInput.val(lastValue + $(this).val());                     // this - элемент, по которому клик, Добавление к текущему значению кликнутой кнопки.  
   });
   
   
   
-  $('.clear').click(function(){
+  // При нажатии на кнопку С.
+  $('.clear').click(function(){                                 
     $('.calc-input').val("0");  
   });
   
+  // При нажатии на = .
+  $('.result').click(function(){
   
   
+  });
   
 });
